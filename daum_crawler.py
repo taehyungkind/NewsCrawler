4365f92594bafe5c5a9b4b8ad3d7d10e7d2bbe41
Hoonjin(Desktop) <bwjhj1030@gmail.com>
2015-08-18 23:53:54 +0900

    다음 파싱 완료. 줌과 다음 모두 id추출시 정규식 사용해야 함

--- daum_crawler.py
+++ daum_crawler.py
@@ -13,14 +13,22 @@ class DaumCrawler(Crawler):
         for category in self.category_list:
             soup = self.get_soup(self.url, {"include": category})
             ul_tag = soup.find("ul", {"class": "list_rank"})
-            self.ul_tag_parser(ul_tag)
+            self.category_news_mapper.setdefault(category, self.ul_tag_parser(ul_tag))
             print()
 
     @staticmethod
     def ul_tag_parser(soup):
         article_list = []
         for li in soup.find_all("li")[:10]:
-            print(li.find("span", {"class": "desc_issue"}).text)
+            article_dict = {}
+            article_dict.setdefault("screen_out", li.find("span", {"class", "screen_out"}).text.strip())
+            thumb = li.find("div", {"class": "cont_thumb"})
+            a_tag = thumb.find("a", {"class": "link_txt"})
+            article_dict.setdefault("title", a_tag.text.strip())
+            href = a_tag.get("href")
+            article_dict.setdefault("href", href)
+            article_dict.setdefault("id", href[3:])
+            article_list.append(article_dict)
 
         return article_list
 
