eb106d826acbbb079258216a16d09afbfe96c511
Hoonjin(Desktop) <bwjhj1030@gmail.com>
2015-08-18 20:27:42 +0900

    Project 이름 변경

--- NewsCrawler/wsgi.py
+++ NewsCrawler/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ZumNewsCrawler project.
+WSGI config for NewsCrawler project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ZumNewsCrawler.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NewsCrawler.settings")
 
 application = get_wsgi_application()
