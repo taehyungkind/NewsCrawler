8e5a89f8c1a663c1fc1af23ea694bbfa52520632
Hoonjin(Desktop) <bwjhj1030@gmail.com>
2015-09-01 00:00:03 +0900

    데이터베이스 테이블에 모두 데이터를 넣음

--- crawl/views.py
+++ crawl/views.py
@@ -1,15 +1,19 @@
 # from django.shortcuts import render
 from .models import *
 from django.shortcuts import HttpResponse
+from django.db import transaction
 from .daum_crawler import DaumCrawler
 from .nate_crawler import NateCrawler
 from .naver_crawler import NaverCrawler
 from .zum_crawler import ZumCrawler
+from django.utils import timezone
 import json
 # Create your views here.
 
-
+@transaction.atomic(using="crawl", savepoint=True)
 def crawl(request, host):
+    ArticleRank.objects.all().update(view=False)
+
     host_dict = {
         "daum": DaumCrawler,
         "nate": NateCrawler,
@@ -17,17 +21,20 @@ def crawl(request, host):
         "zum": ZumCrawler
     }
     crawler = host_dict[host]()
+    crawler.crawl_popular_news_list()
 
     h = Host.objects.get(name=host)
-    crawler.crawl_popular_news_list()
-    print(crawler.category_list)
-    for category in crawler.category_list:
-        print(category)
-        Category(host=h, name=category).save()
-    print(h)
-    news_list = crawler.get_category_news_list(crawler.category_list[0])
 
-    return HttpResponse(json.dumps(news_list))
+    for cate in crawler.category_list:
+        news_list = crawler.get_category_news_list(cate)
+        category = Category.objects.get(host=h, name=cate)
+        for news in news_list:
+            article = Article(id=news['id'], title=news['title'], url=news['url'], host=h, category=category)
+            article.save()
+            ArticleRank(article=article, rank=news['rank'], time=timezone.localtime(timezone.now())).save()
+            print(news)
+
+    return HttpResponse(json.dumps({'status': "ok"}))
 
 
 def test(request):
@@ -47,3 +54,5 @@ def test(request):
         for category in crawler.category_list:
             print(category)
             Category(host=host, name=category).save()
+
+    return HttpResponse(json.dumps({"status": "ok"}))
