b82afa4c20d6e4bb4cc829e53b2ce7ae095ad22f
Hoonjin(Desktop) <bwjhj1030@gmail.com>
2015-09-08 01:45:29 +0900

    크롤 시간 반환키 crawl_time으로 변경. 카테고리 뉴스의 api에도 크롤타입 추가

--- crawl/views.py
+++ crawl/views.py
@@ -19,7 +19,6 @@ from NewsCrawler.tasks import crawling
 
 @transaction.atomic(using="crawl", savepoint=True)
 def crawl(request):
-    print('test')
     crawling()
     return HttpResponse(json.dumps({'status': "ok"}))
 
@@ -31,8 +30,8 @@ def get_category_news(request, host_name, category_name):
 
     # articles = ArticleRank.objects.filter(category=category, view=True).select_related('article')
     # TODO 조인방법을 찾아보자 (딕셔너리로 바꾸고 서브쿼리 보내고 정렬하는 거 없이 한큐에!)
-    rank_list = ArticleRank.objects.filter(category=category, view=True).values("article", "rank")
-    rank_dict = dict([(rank['article'], rank['rank']) for rank in rank_list])
+    rank_list = ArticleRank.objects.filter(category=category, view=True)
+    rank_dict = dict([(rank['article'], rank['rank']) for rank in rank_list.values("article", "rank")])
     articles = Article.objects.filter(id__in=rank_dict.keys())
 
     news_list = []
@@ -42,8 +41,12 @@ def get_category_news(request, host_name, category_name):
             "title": article.title,
             "url": article.url,
         })
-    ordered_news_list = sorted(news_list, key=operator.itemgetter('rank'))
-    return HttpResponse(json.dumps(ordered_news_list))
+
+    result = {
+        'news_list': sorted(news_list, key=operator.itemgetter('rank')),
+        'crawl_time': rank_list.order_by('-time')[0].time.strftime('%Y-%m-%d %H:%M:%S')
+    }
+    return HttpResponse(json.dumps(result))
 
 
 def get_timezone_now():
@@ -62,7 +65,7 @@ def get_host_category_names(request):
         category_list = Category.objects.filter(host=host)
         result[host.name] = [category.name for category in category_list]
 
-    result['last_crawl_time'] = ArticleRank.objects.all().order_by("-time")[0].time.strftime('%Y-%m-%d %H:%M:%S')
+    result['crawl_time'] = ArticleRank.objects.all().order_by("-time")[0].time.strftime('%Y-%m-%d %H:%M:%S')
     return HttpResponse(json.dumps(result))
 
 
