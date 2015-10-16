793b4cbbdcc9c080388fd8874b9033d9050f77e4
HoonJin(Notebook) <bwjhj1030@gmail.com>
2015-10-16 13:52:06 +0900

    간단한 테스트코드 작성. 이것은 python manage.py test crawl.tests.Test1.test로 실행한다

--- crawl/tests.py
+++ crawl/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from crawl.models import *
+from crawl.views import db_initializer
+
+
+class Test1(TestCase):
+    def setUp(self):
+        db_initializer(self)
+
+    def test(self):
+        hosts = Host.objects.all()
+        print(hosts)
+        for host in hosts:
+            categories = Category.objects.filter(host=host)
+            for category in categories:
+                url = "http://localhost:8000/" + host.name + "/" + category.name
+                print(url)
 
 # Create your tests here.
